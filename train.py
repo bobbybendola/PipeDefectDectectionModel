d10d750dc98a9d4b517cf4e4d921cd7cba2796b6
Steven Kuzhipala <steven.k.biji@gmail.com>
2026-06-27 16:24:15 -0700

    Single Epoch run metrics, graphs, and confusion matrix

---
+++ train.py
@@ -0,0 +1,19 @@
+import time #to track the time taken for training
+from ultralytics import YOLO
+
+model= YOLO("yolov8n.pt") #load a pretrained model
+
+print("Start training")
+start=time.time()
+
+model.train(
+      data="data/data.yaml",   #coming from Kaggle Dataset, labelled and marked for each training image
+      epochs=1,
+      imgsz=640,
+      batch=16,
+      device='mps', #specifically for Mac Mini M4 chip, force trainign to GPU 
+      name="pipeline_defect_v1"
+  )
+
+elapsed = time.time() - start
+print(f"\nTraining time: {elapsed:.1f} seconds ({elapsed/60:.1f} minutes)")
